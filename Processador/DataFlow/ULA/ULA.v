06b53fa139e098089a7e23c4d1050680e811e57c
Matheus Guidoni <matheus.guidoni@usp.br>
2023-06-17 20:09:13 -0300

    Adicionada flag_overflow

--- Processador/DataFlow/ULA/ULA.v
+++ Processador/DataFlow/ULA/ULA.v
@@ -1,6 +1,7 @@
 module ULA  #(parameter BITS = 64)
             (dina, dinb, imm, subtraindo, alu_src, operacao,
-             dout, flag_maior_igual_u, flag_igual, flag_menor);
+             dout, flag_maior_igual_u, flag_igual, flag_menor,
+             flag_overflow);
 
 
     input [BITS-1:0] dina, dinb, imm;
@@ -9,7 +10,7 @@ module ULA  #(parameter BITS = 64)
     input [1:0] operacao;
     parameter S = 0, AND = 1, OR = 2;
 
-    output flag_maior_igual_u, flag_igual, flag_menor;
+    output flag_maior_igual_u, flag_igual, flag_menor, flag_overflow;
     output [BITS-1:0] dout, dout_s, dout_and, dout_or;
 
     wire [BITS-1:0] fator_1, fator_2;
@@ -30,7 +31,7 @@ module ULA  #(parameter BITS = 64)
     /* OR */
     assign dout_or = fator_1 | fator_2;
 
-    SomadorSubtrator soma_sub(.subtraindo(subtraindo), .A(fator_1), .B(fator_2), .S(dout_s));
+    SomadorSubtrator soma_sub(.subtraindo(subtraindo), .A(fator_1), .B(fator_2), .S(dout_s), .cout(flag_overflow));
 
     Flags flags(.dinA(dina), .dinB(dinb), .flag_maior_igual_u(flag_maior_igual_u),
                 .flag_igual(flag_igual), .flag_menor(flag_menor));
